e9a35323e34adcdb8ea51510be678c21d2f70af1
Abram Ashraf <77212440+Abrammse@users.noreply.github.com>
2024-02-11 14:04:50 +0200

    Update settings.py

--- stschool/settings.py
+++ stschool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-s)h77nte)9_d##2@(!w-$1%9-6feigjzfkpu)&g^mmi@m_dl@g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
